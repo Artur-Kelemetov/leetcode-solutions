612b6f8f6021ba44551f132414d564295862ede7
Artur-Kelemetov <tenesly14@gmail.com>
2026-05-15 18:25:24 +0300

    Time: 3 ms (55.24%), Space: 55.4 MB (34.91%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const seen = new Map()
+
+    for (let i = 0; i < nums.length; i++) {
+        const current = nums[i]
+        const need = target - current
+
+        if (seen.has(need)) {
+            return [seen.get(need), i]
+        }
+
+        seen.set(current, i)
+    }
+};
