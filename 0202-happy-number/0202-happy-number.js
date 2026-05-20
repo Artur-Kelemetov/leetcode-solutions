d41dd12209f3c69fd1acf29a24d96f70c389151d
Artur-Kelemetov <tenesly14@gmail.com>
2026-05-20 23:43:17 +0300

    Time: 1 ms (75.51%), Space: 56.7 MB (23.13%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isHappy = function(n) {
+    const seen = new Set()
+
+    while (n !== 1) {
+        if (seen.has(n)) {
+            return false
+        }
+
+        seen.add(n)
+        n = getNext(n)
+    }
+
+    return true
+};
+
+function getNext(num) {
+    let sum = 0
+
+    while (num > 0) {
+        const digit = num % 10
+        sum += digit * digit
+        num = Math.floor(num / 10)
+    }
+
+    return sum
+}
