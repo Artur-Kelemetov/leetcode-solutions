c3f2fcb1477c86e18f991ab897e16351b4ede7d9
Artur-Kelemetov <tenesly14@gmail.com>
2026-05-21 21:58:28 +0300

    Time: 3 ms (95.57%), Space: 54.5 MB (76.65%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isIsomorphic = function(s, t) {
+    const mapST = new Map()
+    const mapTS = new Map()
+
+    for (let i = 0; i < s.length; i++) {
+        const charS = s[i]
+        const charT = t[i]
+
+        if (mapST.has(charS) && mapST.get(charS) !== charT) {
+            return false
+        }
+
+        if (mapTS.has(charT) && mapTS.get(charT) !== charS) {
+            return false
+        }
+
+        mapST.set(charS, charT)
+        mapTS.set(charT, charS)
+    }
+
+    return true
+};
